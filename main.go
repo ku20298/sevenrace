0d148a0bb72da5172e789d973a36e7b68c6e7de8
ku20298 <31235499+ku20298@users.noreply.github.com>
2018-04-26 10:10:30 +0900

    Update main.go

--- main.go
+++ main.go
@@ -104,13 +104,13 @@ func createCourse() {
 
 			if count == 0 {
 				if i > 180 {
-					time.Sleep(time.Millisecond * 13)
+					time.Sleep(time.Millisecond * 12)
 					raceCourse.data = append(raceCourse.data[1:], float64(raceCourse.positionX))	
 				}else {
 					raceCourse.data = append(raceCourse.data, float64(raceCourse.positionX))
 				}
 			}else {
-				time.Sleep(time.Millisecond * 13)
+				time.Sleep(time.Millisecond * 12)
 				raceCourse.data = append(raceCourse.data[1:], float64(raceCourse.positionX))	
 			}
 			
